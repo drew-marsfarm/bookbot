587b83ecb5e7f8040acd3c461760150ffc19ad84
Drew Thomas <drew@marsfarm.io>
2025-08-24 13:23:26 -0500

    function added

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as book:
+        book_contents = book.read()
+    return book_contents 
+
+if __name__  == "__main__":
+    output = get_book_text("./books/frankenstein.txt")
+    print(output)
