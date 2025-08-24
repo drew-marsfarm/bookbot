98c3737595ea48f87d76be18f47961bd7a0f6d42
Drew Thomas <drew@marsfarm.io>
2025-08-24 14:27:32 -0500

    added letter count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import count_letters
 
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -9,5 +10,6 @@ if __name__  == "__main__":
     output = get_book_text("./books/frankenstein.txt")
     
     word_count_output = word_count(output)
+    count_letters_output = count_letters(output)
     
-    print(f"{word_count_output} words found in the document")
+    print(f"{word_count_output} words found in the document /n {count_letters_output}")
--- stats.py
+++ stats.py
@@ -1,4 +1,11 @@
 def word_count(string):
-    process1 = string.split()
-    length = len(process1)
-    return length
+    separate_words_to_list = string.split()
+    count = len(separate_words_to_list)
+    return count
+
+def count_letters(string):
+    lowercase = string.lower()
+    letters = ["a", "b", "C", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    for letter in letters:
+        instance = lowercase.count(letter)
+        print(f"{letter}: {instance}")
