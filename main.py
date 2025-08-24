3cf9c768604b33eb2bddbb0ead3cc632a9c08a6b
Drew Thomas <drew@marsfarm.io>
2025-08-24 13:41:04 -0500

    cleaned up structure

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def get_book_text(filepath):
     return book_contents 
 
 if __name__  == "__main__":
-    #output = get_book_text("./books/frankenstein.txt")
-    with open("./books/frankenstein.txt") as book:
-        book_contents = book.read()
-        output = word_count(book_contents)
-    print(f"{output} words found in the document")
+    output = get_book_text("./books/frankenstein.txt")
+    
+    word_count_output = word_count(output)
+    
+    print(f"{word_count_output} words found in the document")
