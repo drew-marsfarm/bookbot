ac1b3eb3e344f9a329e5894ff1aaa6be33010b7b
Drew Thomas <drew@marsfarm.io>
2025-08-24 13:38:16 -0500

    refactored function structure

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import word_count
+
 def get_book_text(filepath):
     with open(filepath) as book:
         book_contents = book.read()
     return book_contents 
-
-def word_count(string):
-    process1 = string.split()
-    length = len(process1)
-    return length
 
 if __name__  == "__main__":
     #output = get_book_text("./books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(string):
+    process1 = string.split()
+    length = len(process1)
+    return length
