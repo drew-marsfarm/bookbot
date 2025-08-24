c5aa146876f3ac23de4918da7e86072ceefc91b1
Drew Thomas <drew@marsfarm.io>
2025-08-24 15:30:36 -0500

    sorted character count and ux

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import word_count
 from stats import count_letters
+from stats import sort_dict
 
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -11,6 +12,18 @@ if __name__  == "__main__":
     
     word_count_output = word_count(output)
     count_letters_output = count_letters(output)
-    
-    print(f"{word_count_output} words found in the document")
-    print(count_letters_output)
+    character_count_output = sort_dict(count_letters_output)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count_output} total words")
+    print("--------- Character Count -------")
+
+    for char in character_count_output:
+        # each 'char' is a dictionary: {"char": "a", "num": 234}
+        letter = char["char"]
+        number = char["num"]
+        print(f"{letter}: {number}")
+
+    print("============= END ===============")
--- stats.py
+++ stats.py
@@ -5,36 +5,25 @@ def word_count(string):
 
 def count_letters(string):
     lowercase = string.lower()
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    letter_dict = {
-        "a": 0,
-        "b": 0,
-        "c": 0, 
-        "d": 0, 
-        "e": 0, 
-        "f": 0, 
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0,
-        }
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "â", "ê", "ë", "ô"]
+    letter_dict = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0, "æ": 0, "â": 0, "ê": 0, "ë": 0, "ô": 0}
     for letter in letters:
         instance = lowercase.count(letter)
         letter_dict[letter] = instance
     return letter_dict
+
+def sort_dict(dictionary):
+    def sort_on(items):
+        return items["num"]
+    
+    unsorted_dict = []
+
+    for char in dictionary:
+        number = dictionary[char]
+        unsorted_dict.append(
+            {"char": char, "num": number}
+        )
+
+    sorted_dict = unsorted_dict.sort(reverse=True, key=sort_on)
+    
+    return sorted_dict
