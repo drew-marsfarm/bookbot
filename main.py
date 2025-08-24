a3438623abb470c9eb8a87abb678513cf443df14
Drew Thomas <drew@marsfarm.io>
2025-08-24 13:34:00 -0500

    added word_count

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def get_book_text(filepath):
         book_contents = book.read()
     return book_contents 
 
+def word_count(string):
+    process1 = string.split()
+    length = len(process1)
+    return length
+
 if __name__  == "__main__":
-    output = get_book_text("./books/frankenstein.txt")
-    print(output)
+    #output = get_book_text("./books/frankenstein.txt")
+    with open("./books/frankenstein.txt") as book:
+        output = word_count(book)
+    print(f"{output} words found in the document")
